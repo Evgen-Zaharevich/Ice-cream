aa8a7926e4e47b2b7b3d2c1d50965d468a20c8c1
Evgen Zaharevich <metrovyy@gmail.com>
2023-03-27 23:28:30 +0300

    finish

--- src/js/mobile-menu.js
+++ src/js/mobile-menu.js
@@ -20,3 +20,57 @@
     bodyScrollLock.enableBodyScroll(document.body);
     });
 })();
+
+// (() => {
+//   const menuBtnsRef = document.querySelectorAll('[data-menu-button]');
+//   const mobileMenuRef = document.querySelector('[data-menu]');
+
+//   for (const btn in menuBtnsRef) {
+//     if (Object.hasOwnProperty.call(menuBtnsRef, btn)) {
+//       const element = menuBtnsRef[btn];
+//       console.log(element);
+
+//       element.addEventListener('click', () => {
+//         toogleClass();
+//       });
+//     }
+//   }
+
+//   // Close the mobile menu on wider screens if the device orientation changes
+//   window.matchMedia('(min-width: 1199px)').addEventListener('change', e => {
+//     if (!e.matches) return;
+//     mobileMenuRef.classList.remove('is-open');
+
+//     closeModal();
+
+//     // bodyScrollLock.enableBodyScroll(document.body);
+//   });
+
+//   function closeModal() {
+//     console.log('close');
+//     for (const btn in menuBtnsRef) {
+//       if (Object.hasOwnProperty.call(menuBtnsRef, btn)) {
+//         const element = menuBtnsRef[btn];
+//         console.log(element);
+//         element.setAttribute('aria-expanded', false);
+//         element.classList.remove('is-open');
+//       }
+//     }
+//   }
+
+//   function toogleClass() {
+    
+//     for (const btn in menuBtnsRef) {
+//       if (Object.hasOwnProperty.call(menuBtnsRef, btn)) {
+//         const element = menuBtnsRef[btn];
+//         const expanded =
+//         element.getAttribute('aria-expanded') === 'true' || false;
+//         element.classList.toggle('is-open');
+//         element.setAttribute('aria-expanded', !expanded);
+//       }
+//     }
+
+//     mobileMenuRef.classList.toggle('is-open');
+//   }
+
+// })();
--- src/js/scroll.js
+++ src/js/scroll.js
@@ -3,6 +3,7 @@ import $ from 'jquery';
 $(document).ready(function () {
   // Add smooth scrolling to all links
   $('a').on('click', function (event) {
+    
     // Make sure this.hash has a value before overriding default behavior
     if (this.hash !== '') {
       // Prevent default anchor click behavior
